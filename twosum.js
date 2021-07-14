81f0fd0d58e6cb6acc16fa6fd011478e89550554
ericabasak <ericabasak@gmail.com>
2021-07-14 16:17:14 -0400

    two sum problem

--- twosum.js
+++ twosum.js
@@ -14,7 +14,10 @@ function twoSum(nums, target) {
   let arr = [];
   for(let i = 0; i < nums.length; i++){
     for(let j = i + 1; j < nums.length; j++){
-      if(nums[i] + nums[j])
+      if(nums[i] + nums[j] === target){
+        arr.push(i, j);
+      }
     }
   }
+  return arr;
 }
